c8ffb1d536f629e0acb9fab733d1ac5905fc5958
aakashaman <aakashaman9931@gmail.com>
2023-06-21 17:50:32 +0530

    see

--- src/Form.js
+++ src/Form.js
@@ -16,7 +16,7 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch("http://68.183.113.165:5000/add-credentials", {
+    fetch("https://68.183.113.165:5000/add-credentials", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
